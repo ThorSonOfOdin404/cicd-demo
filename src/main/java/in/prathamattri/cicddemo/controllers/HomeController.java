90ecc0e8cb9fbd2ec71efc4c097738fc9e202668
Pratham Attri <attripratham2062@gmail.com>
2026-09-26 17:42:00 +0530

    - Added styling to home page and greeting page. - Updated to thymeleaf greeting.html page instead of a simple ResponseEntity for greeting endpoint

--- src/main/java/in/prathamattri/cicddemo/controllers/HomeController.java
+++ src/main/java/in/prathamattri/cicddemo/controllers/HomeController.java
@@ -1,23 +1,25 @@
 package in.prathamattri.cicddemo.controllers;
 
 import in.prathamattri.cicddemo.entity.UserDetails;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "home";
     }
 
     @PostMapping("/greeting")
-    @ResponseBody
-    public ResponseEntity<String> greeting(@ModelAttribute UserDetails userDetails){
-        return ResponseEntity.ok(String.format("Hello %s", userDetails.getFullName()));
+    public String greeting( @ModelAttribute UserDetails userDetails, Model model) {
+
+        model.addAttribute("name", userDetails.getFullName());
+        return "greeting";
     }
 }
+
 
